be1c66965d9222453489172c53839ca919e35124
giga487 <giga487@hotmail.com>
2021-05-31 22:42:57 +0200

    q data stream inserito

--- dataStream/main.cpp
+++ dataStream/main.cpp
@@ -1,11 +1,34 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QFile>
 
 int main(int argc, char *argv[])
 {
   QApplication a(argc, argv);
   MainWindow w;
+  QDataStream *in;
+
+
+
+  QFile file("D:\\Git\\QT_Example\\dataStream\\img.jpg");
+
+  if(file.exists() == true)
+  {
+    file.open(QIODevice::ReadOnly);
+    qDebug("File aperto");
+  }
+
+
+  in = new QDataStream(&file);   // we will serialize the data into the file
+  in->setByteOrder(QDataStream::LittleEndian);
+
+  qDebug() << "Data" << in->device()->pos();
+  qDebug() << "data content after write: " << file.readAll();
+
+  in->device()->seek(0); // mi posiziono sull'elemento zero
+  qDebug() << "Device pos after seek: " << in->device()->pos();
+
   w.show();
   return a.exec();
 }
